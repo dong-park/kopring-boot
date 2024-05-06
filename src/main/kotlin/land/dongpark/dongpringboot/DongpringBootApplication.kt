e57b2f566e4367430c5fb1c8b83b710ac740307d
dong-park <rakddh@gmail.com>
2024-05-07 00:30:22 +0900

    ADD: init project

---
+++ src/main/kotlin/land/dongpark/dongpringboot/DongpringBootApplication.kt
@@ -0,0 +1,12 @@
+package land.dongpark.dongpringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<DongpringBootApplication>(*args)
+
+}
---
+++ src/test/kotlin/land/dongpark/dongpringboot/DongpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package land.dongpark.dongpringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DongpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
